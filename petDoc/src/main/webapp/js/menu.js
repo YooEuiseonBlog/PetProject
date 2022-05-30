eef67ce0d2a8c90de7b9c4d3b2adb2f6476b5b9f
LeeChaeKyoung <jokerrabbit0712@naver.com>
2022-05-30 19:25:56 +0900

    Top 3

--- petDoc/src/main/webapp/js/menu.js
+++ petDoc/src/main/webapp/js/menu.js
@@ -7,3 +7,23 @@ function activeLink() {
 
 list.forEach((item) =>
     item.addEventListener('click', activeLink));
+    
+const Top1 = document.querySelector('.Top_logo');  
+const Top2 = document.querySelector('#Top_profile');
+const Top3 = document.querySelector('#Top_info');  
+ function changeOpacity1(){
+	Top1.style.opacity = "0.0";
+	Top2.style.opacity = "0.0";
+	Top3.style.opacity = "0.0";
+}
+
+function changeOpacity2(){
+	Top1.style.opacity = "1";
+	Top2.style.opacity = "1";
+	Top3.style.opacity = "1";
+}
+
+const con = document.querySelector('.Top_container');
+
+con.onmouseover=changeOpacity2;
+con.onmouseout=changeOpacity1;
