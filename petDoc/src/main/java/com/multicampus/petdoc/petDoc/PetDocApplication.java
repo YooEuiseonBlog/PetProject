29af710daba9575f39937400e4b30655bf1755ec
Yoo eui seon <yesman5307@gmail.com>
2022-05-30 11:59:50 +0900

    project file
    upload project file

---
+++ petDoc/src/main/java/com/multicampus/petdoc/petDoc/PetDocApplication.java
@@ -0,0 +1,13 @@
+package com.multicampus.petdoc.petDoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetDocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetDocApplication.class, args);
+	}
+
+}
---
+++ petDoc/src/test/java/com/multicampus/petdoc/petDoc/PetDocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multicampus.petdoc.petDoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetDocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
