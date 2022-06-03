805305dd7b12dbc54ccb759a4ff39cf2aea55a69
Yooesuiseon <kgh4464@kakao.com>
2022-06-03 14:45:24 +0900

    commit main

--- petDoc/src/main/java/com/multicampus/petdoc/petDoc/vo/MapVO.java
+++ petDoc/src/main/java/com/multicampus/petdoc/petDoc/vo/MapVO.java
@@ -10,5 +10,84 @@ public class MapVO {
 	private String hos_tel;
 	private String hos_addr_old;
 	private String hos_addr_new;
+	private String hos_name;
+	private double hos_areaX;
+	private double hos_areaY;
+	public int getHos_no() {
+		return hos_no;
+	}
+	public void setHos_no(int hos_no) {
+		this.hos_no = hos_no;
+	}
+	public String getHos_servicename() {
+		return hos_servicename;
+	}
+	public void setHos_servicename(String hos_servicename) {
+		this.hos_servicename = hos_servicename;
+	}
+	public int getHos_managenum() {
+		return hos_managenum;
+	}
+	public void setHos_managenum(int hos_managenum) {
+		this.hos_managenum = hos_managenum;
+	}
+	public String getHos_status() {
+		return hos_status;
+	}
+	public void setHos_status(String hos_status) {
+		this.hos_status = hos_status;
+	}
+	public String getHos_status_name() {
+		return hos_status_name;
+	}
+	public void setHos_status_name(String hos_status_name) {
+		this.hos_status_name = hos_status_name;
+	}
+	public String getHos_status_code() {
+		return hos_status_code;
+	}
+	public void setHos_status_code(String hos_status_code) {
+		this.hos_status_code = hos_status_code;
+	}
+	public String getHos_tel() {
+		return hos_tel;
+	}
+	public void setHos_tel(String hos_tel) {
+		this.hos_tel = hos_tel;
+	}
+	public String getHos_addr_old() {
+		return hos_addr_old;
+	}
+	public void setHos_addr_old(String hos_addr_old) {
+		this.hos_addr_old = hos_addr_old;
+	}
+	public String getHos_addr_new() {
+		return hos_addr_new;
+	}
+	public void setHos_addr_new(String hos_addr_new) {
+		this.hos_addr_new = hos_addr_new;
+	}
+	public String getHos_name() {
+		return hos_name;
+	}
+	public void setHos_name(String hos_name) {
+		this.hos_name = hos_name;
+	}
+	public double getHos_areaX() {
+		return hos_areaX;
+	}
+	public void setHos_areaX(double hos_areaX) {
+		this.hos_areaX = hos_areaX;
+	}
+	public double getHos_areaY() {
+		return hos_areaY;
+	}
+	public void setHos_areaY(double hos_areaY) {
+		this.hos_areaY = hos_areaY;
+	}
+	
+	
+	
+	
 	
 }
